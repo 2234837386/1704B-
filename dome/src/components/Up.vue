02af30ce1e531cb23b0d852e98c2dc1601681b4b
2234837386 <2234837386@qq.com>
2019-11-30 10:55:54 +0800

    弹窗

---
+++ dome/src/components/Up.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="up" :class="{active:ismask}"></div>
+</template>
+
+<script>
+export default {
+     props:{ismask:{
+         type:Boolean
+     }}
+};
+</script>
+
+<style lang="scss" scoped>
+#up {
+  width: 100%;
+  height: 100%;
+  background: #ffffff;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  transform: translateY(100%);
+  transition: all 1s ease;
+  &.active {
+    transform: translateY(0%);
+  }
+}
+</style>
--- dome/src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>更新代码</h1>
-  </div>
-</template>
