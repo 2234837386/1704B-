2258a534a15ed2b5174aa6b96207eff0401053ac
hammer <hammergwx@163.com>
2019-12-02 19:55:30 +0800

    edit details2.0

--- dome/src/App.vue
+++ dome/src/App.vue
@@ -1,11 +1,5 @@
 <template>
-<<<<<<< HEAD
-
-    <router-view/>
-
-=======
-    <router-view/>
->>>>>>> 13cc49a2a7f5f0c090af92652918fd2a034545af
+  <router-view />
 </template>
 <script>
 import { mapActions } from "vuex";
