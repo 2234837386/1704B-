7054008cdf973f2e057952268fe6a904bd8e69c4
MisakiChen125 <1056832687@qq.com>
2019-12-03 20:38:46 +0800

    imgs wancheng

--- dome/src/views/pictures/index.vue
+++ dome/src/views/pictures/index.vue
@@ -10,8 +10,30 @@
         <i class="iconfont icon-angle-down"></i>
       </li>
     </div>
-
-    <!-- <router-view></router-view> -->
+    <div class="boxList">
+      <div class="item" v-for="(item,index) in list" :key="index">
+        <div
+          class="main"
+          v-for="(k,i) in item.List"
+          :key="i"
+          v-show="i===0"
+          :data-id="i"
+          v-lazy:background-image="k.Url"
+        >
+         <div class="mask">
+            <p>{{item.Name}}</p>
+          <p>{{item.Count}}张></p>
+         </div>
+        </div>
+        <div
+          class="main"
+          v-for="(k,i) in item.List"
+          :key="i"
+          v-show="i!==0"
+          v-lazy:background-image="k.Url"
+        ></div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -21,17 +43,27 @@ export default {
   data() {
     return {
       id: "",
-      color:'颜色',
-      cars:'车款'
+      color: "颜色",
+      cars: "车款",
+      list: [],
+      imgs: []
     };
   },
   computed: {},
   created() {
     this.color = this.$route.query.colorName;
-    this.cars=this.$route.query.carName
-    console.log(this.$route.query.colorName)
+    this.cars = this.$route.query.carName;
+    console.log(this.$route.query.colorName);
     this.id = this.$route.query.SerialID;
-    // console.log(this.id)
+    this.$http
+      .get(
+        `http://baojia.chelun.com/v2-car-getImageList.html?SerialID=${this.$route.query.SerialID}`
+      )
+      .then(res => {
+        this.list = res.data.data;
+        this.imgs = this.list.List;
+        console.log(this.list);
+      });
   },
   methods: {
     carcolor() {
@@ -66,6 +98,7 @@ export default {
     height: 50px;
     line-height: 50px;
     display: flex;
+    flex-shrink: 0;
     li {
       flex: 1;
       text-align: center;
@@ -73,6 +106,53 @@ export default {
     }
     li:nth-child(1) {
       border-right: 1px solid #ccc;
+    }
+  }
+  .boxList {
+    width: 100%;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    background: #fff;
+    .item {
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      padding-bottom: 0.2rem;
+      box-sizing: border-box;
+      .main {
+        width: calc(100% / 3);
+        height: 7.6rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0.08rem;
+        box-sizing: border-box;
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        flex-direction: column;
+        .mask{
+          width: 100%;
+          height: 7.6rem;
+          display: flex;
+          flex-direction: column;
+          background: rgba(0,0,0,.5);
+          padding: 25px;
+          p {
+          width: 100%;         
+          color:#fff;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-style: normal;
+          // font-weight: 1px;
+        }
+        p:first-child{
+          margin-bottom: 10px;
+        }
+        }
+        
+      }
     }
   }
 }
