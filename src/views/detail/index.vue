aaecbdda5375293509b5d8131170edfe4ec27e6b
hammer <hammergwx@163.com>
2019-12-05 15:42:03 +0800

    edit detailTab

--- src/views/detail/index.vue
+++ src/views/detail/index.vue
@@ -2,16 +2,16 @@
   <div class="detail_page">
     <!-- {{Alllist}} -->
     <!-- 点击img跳转到pictures页面 -->
-    <img class="banimg" :src="Alllist.CoverPhoto" @click="tiaoimg(Alllist)"/>
+    <img class="banimg" :src="Alllist.CoverPhoto" @click="tiaoimg(Alllist)" />
     <div class="carmsg">
       <p>
-        <b v-if="Alllist.market_attribute">{{Alllist.market_attribute.dealer_price}}</b> 
+        <b v-if="Alllist.market_attribute">{{Alllist.market_attribute.dealer_price}}</b>
         <span v-if="Alllist.market_attribute">指导价：{{Alllist.market_attribute.official_refer_price}}</span>
       </p>
 
       <button v-if="Alllist">{{Alllist.BottomEntranceTitle}}</button>
     </div>
-     <div class="timemsg">
+    <div class="timemsg">
       <span
         :class="{active:ind===index}"
         v-for="(item,index) in getYear"
@@ -19,7 +19,7 @@
         @click="()=>ind=index"
       >{{item}}</span>
     </div>
-      <div class="inhale_type" v-for="(item,index) in listEach" :key="index">
+    <div class="inhale_type" v-for="(item,index) in listEach" :key="index">
       <div class="title">{{item.exhaust_str}}/{{item.max_power_str}}{{item.inhale_type}}</div>
       <div class="text">
         <span>{{item.market_attribute.year}}款 {{item.car_name}}</span>
@@ -44,13 +44,13 @@ export default {
   props: {},
   components: {},
   data() {
-      return {
-          ind:0
-      }
+    return {
+      ind: 0
+    };
   },
   computed: {
     ...mapState({
-       Alllist: state => state.detail.allList
+      Alllist: state => state.detail.allList
     }),
     getYear() {
       let arr = ["全部"];
@@ -62,7 +62,7 @@ export default {
         });
       return arr;
     },
-        listEach() {
+    listEach() {
       let list = JSON.stringify(this.Alllist.list);
       return (
         list &&
@@ -77,26 +77,24 @@ export default {
     ...mapActions({
       carList: "detail/carList"
     }),
-    tiaoimg(Alllist){
+    tiaoimg(Alllist) {
       this.$router.push({
         path: "/picture",
-        query :{
+        query: {
           SerialID: this.$route.params.id
         }
       });
     }
   },
   created() {
-    
     this.carList(this.$route.params.id);
   },
   mounted() {}
 };
 </script>
 <style lang="scss" scoped>
-.detail_page{
-   
-    width: 100%;
+.detail_page {
+  width: 100%;
   height: 100%;
   display: flex;
 
@@ -129,7 +127,7 @@ export default {
       }
       span {
         color: #ccc;
-        font-size: .7rem;
+        font-size: 0.7rem;
       }
     }
     button {
@@ -152,12 +150,9 @@ export default {
     flex-shrink: 0;
     span {
       margin: 0 10px;
-      &active {
+      &.active {
         color: #09f;
       }
-    }
-    span:first-child {
-      color: #09f;
     }
     .inhale_type {
       width: 100%;
@@ -165,7 +160,6 @@ export default {
       flex-direction: column;
     }
   }
-  
 }
 .inhale_type {
   background: #eee;
@@ -242,5 +236,4 @@ export default {
 .ibtn {
   font-style: normal;
 }
-
 </style>
