400ccdb889426ddc9f1555feb742d529efb5f6b7
hammer <hammergwx@163.com>
2019-12-12 10:23:37 +0800

    update

--- src/servicer/module/city.js
+++
@@ -1,9 +0,0 @@
-
-// 引入拦截器
-import request from "@/util/request"
-// 全国列表
-
-export let getCityList=(id)=>{
-    return request("/v1-city-alllist.html",{params:{provinceid:id}})
-}
-
--- src/servicer/module/form.js
+++
@@ -1,4 +0,0 @@
-import request from "@/util/request";
-export let getformList = () => {
-    return request("v2-dealer-alllist.html?carId=131315&cityId=201")
-}
