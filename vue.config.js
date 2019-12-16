035167e7716e7fbb4be30b489bf08e71ae692881
2234837386 <2234837386@qq.com>
2019-12-16 19:56:48 +0800

    正则判断

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-module.exports = {
-    publicPath:'/1704B/liumeiqin/',
-    productionSourceMap:false  
-  }
+// module.exports = {
+//     publicPath:'/1704B/liumeiqin/',
+//     productionSourceMap:false  
+//   }
